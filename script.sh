cfddf8852925fc26e7c56912e46d9f2b8633006d
Igor <igor.majerczak@student.uj.edu.pl>
2022-10-12 13:25:59 +0200

    stage 2 - actual conversion and ability to use floats

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 definitionRegex="^[a-zA-Z]+_to_[a-zA-Z]+$"
 conversionRateRegex="^-?[0-9]+\.?[0-9]*$"
@@ -8,9 +8,13 @@ inputLength="${#user_input[@]}"
 definition=${user_input[0]}
 conversionRate=${user_input[1]}
 if [[ "$definition" =~ $definitionRegex ]] && [[ "$conversionRate" =~ $conversionRateRegex ]] && [[ "$inputLength" == 2 ]]; then
-	echo "The definition is correct!"
+	echo "Enter a value to convert:"
+	read value
+	while [[ ! "$value" =~ $conversionRateRegex ]]; do
+		echo "Enter a float or integer value!"
+		read value
+	done
+	echo Result: $( bc -l <<< "$value * $conversionRate" ) 
 else
 	echo "The definition is incorrect!"
 fi
-
-
